b6e34ed3ad943a3978e1c9b5b9d21cd8fdbf76bf
Conor <cmcnamee4@gmail.com>
2021-11-14 21:03:41 +0000

    Added to functionality to create an assignment

--- frontend/src/components/assignment/assignmentForm.tsx
+++ frontend/src/components/assignment/assignmentForm.tsx
@@ -1,34 +1,39 @@
-import react from "react";
+import react, { useState } from "react";
 import "./assignment_form.css";
-import axios, {AxiosResponse} from 'axios';
+import axios from "axios";
+import React from "react";
 
-const assignmentForm = () => {
 
+const AssignmentForm = () => {
+
+    const [assignmentTitle, setAssignmentTitle] = useState('');
     const onSubmit = () => {
         axios({
           method: 'post',
-          url: "http://127.0.0.1:8000/assignments/set-assignments/",
+          url: "http://127.0.0.1:8000/assignments/create-assignment/",
           data: {
-            
+            assignment_title: assignmentTitle,
           }
         })
+        alert(assignmentTitle);
       }
     
-      const onChange = () => {
-        console.log("E")
+      const handleChange = (e: React.FormEvent<HTMLInputElement>):void => {
+        setAssignmentTitle(e.currentTarget.value);
       }
 
     return(
         <div className="assignment-form-container">
             <h1 className="assignment-header">Add Assignment</h1>
-            <form className="assignment-form">
+            <form className="assignment-form" onSubmit={onSubmit}>
                 <input
                     className="assignment-form-input"
                     type="text" 
+                    onChange={handleChange}
                 />
             </form>
         </div>
     )
 }
 
-export default assignmentForm;
+export default AssignmentForm;
--- frontend/src/components/assignment/assignments.tsx
+++ frontend/src/components/assignment/assignments.tsx
@@ -2,6 +2,7 @@ import { render } from '@testing-library/react';
 import {useEffect, useState} from 'react';
 import useFetch from "../hooks/hooks";
 import AssignmentForm from "./assignmentForm";
+import axios, {AxiosResponse} from 'axios';
 
 import "./assignments.css";
 
@@ -9,7 +10,20 @@ const Assignments = () => {
 
   const [data, loading] = useFetch("http://127.0.0.1:8000/assignments/get-assignments/");
   const [assignmentName, setAssignmentName] = useState();
+  
+  const onSubmit = () => {
+    axios({
+      method: 'post',
+      url: "http://127.0.0.1:8000/assignments/set-assignments/",
+      data: {
+        
+      }
+    })
+  }
 
+  const onChange = () => {
+    console.log("E")
+  }
 
   console.log(data);
   return(
@@ -18,9 +32,10 @@ const Assignments = () => {
         <div className="assignment-list-item">
           {
             data.map((assignment) => <div key={assignment.assignment_id} draggable="true" className="assignment-list-item-container">
-                <p className="assignment">{assignment.assignment_title}</p>
+                <p contentEditable="true" className="assignment">{assignment.assignment_title}</p>
                 <input type="checkbox"></input>
               </div>
+
             )
           }
         </div>
