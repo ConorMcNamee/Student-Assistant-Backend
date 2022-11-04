fffad2208ae6955d6d0e6019deaf16ee1d82c21e
ConorMcNamee <cmcnamee4@gmail.com>
2022-11-04 20:02:15 +0000

    Added PostgreSQL to Dockerfile and docker compose

--- student_assist/student_assist/settings.py
+++ student_assist/student_assist/settings.py
@@ -84,16 +84,16 @@ WSGI_APPLICATION = 'student_assist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': config('NAME'),
-#         'USER': config('USER'),
-#         'PASSWORD': config('PASSWORD'),
-#         'HOST': config('HOST'),
-#         'PORT': config('PORT'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        'NAME': os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+        'USER': os.environ.get("SQL_USER", "user"),
+        'PASSWORD': os.environ.get("SQL_PASSWORD", "password"),
+        'HOST': os.environ.get("SQL_HOST", "localhost"),
+        'PORT': os.environ.get("SQL_PORT", "5432"),
+    }
+}
 
 
 # Password validation
